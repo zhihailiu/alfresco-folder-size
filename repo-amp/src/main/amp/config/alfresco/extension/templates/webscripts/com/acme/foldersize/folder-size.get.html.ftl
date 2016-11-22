cb8f67644a70453675a579942c225b9cf389e6f0
Zhihai Liu <Zhihai Liu>
2016-11-22 12:54:03 -0500

    Add repo webscript to calculate folder size

---
+++ repo-amp/src/main/amp/config/alfresco/extension/templates/webscripts/com/acme/foldersize/folder-size.get.html.ftl
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Folder Size</title>
+</head>
+<body>
+        <div>
+        <h1>Folder Size</h1>
+        <p>Name : ${nodeName}</p>
+        <p>Size : ${size}</p>
+    </div>
+</body>
+</html>
---
+++ repo-amp/src/main/java/com/acme/foldersize/FolderSizeWebScript.java
@@ -0,0 +1,63 @@
+package com.acme.foldersize;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.ContentData;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.springframework.extensions.webscripts.Cache;
+import org.springframework.extensions.webscripts.DeclarativeWebScript;
+import org.springframework.extensions.webscripts.Status;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+
+public class FolderSizeWebScript extends DeclarativeWebScript {
+
+	private NodeService nodeService;
+
+	public final void setNodeService(final NodeService nodeService) {
+		this.nodeService = nodeService;
+	}
+
+	@Override
+	protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		String nodeRefId = req.getParameter("nodeRef");
+		NodeRef nodeRef = new NodeRef(nodeRefId);
+
+		String nodeName = (String) nodeService.getProperty(nodeRef, ContentModel.PROP_NAME);
+
+		long size = getNodeSize(nodeRef);
+
+		model.put("nodeName", nodeName);
+		model.put("size", Long.toString(size));
+
+		return model;
+	}
+
+	private long getNodeSize(NodeRef nodeRef) {
+		long size = 0;
+
+		// Collecting current node size
+		ContentData contentData = (ContentData) nodeService.getProperty(nodeRef, ContentModel.PROP_CONTENT);
+		try {
+			size = contentData.getSize();
+		} catch (Exception e) {
+			size = 0;
+		}
+
+		// Collecting child nodes' sizes
+		List<ChildAssociationRef> chilAssocsList = nodeService.getChildAssocs(nodeRef);
+
+		for (ChildAssociationRef childAssociationRef : chilAssocsList) {
+			NodeRef childNodeRef = childAssociationRef.getChildRef();
+			size = size + getNodeSize(childNodeRef);
+		}
+
+		return size;
+	}
+
+}
